75ad3f0def03f674d85d8857fae7764904aa8361
MuhammedGul34 <muhammedgul.mail@gmail.com>
2022-12-14 00:19:36 +0300

    Final V

--- DıgerUygulamalarlaTextPaylasma/ViewController.swift
+++ DıgerUygulamalarlaTextPaylasma/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbllMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnShareClicked(_ sender: UIButton) {
+        let activityViewController = UIActivityViewController(activityItems: [lbllMessage.text!], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: nil)
+        
+        
+    }
 }
 
